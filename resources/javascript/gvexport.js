62e4168780c9a2e04f0ce4680e586efce844fa58
Neriderc <89974636+Neriderc@users.noreply.github.com>
2022-10-07 20:56:49 +1300

    Add X for closing fullscreen
    Instead of having the same icon, now an X shows to close fullscreen mode.
    
    Also contains two minor changes:
     - Fix for webtrees footer being partially covered
    - Fix for links not opening in new tab in some cases

--- resources/javascript/gvexport.js
+++ resources/javascript/gvexport.js
@@ -529,8 +529,7 @@ function updateClearAll() {
 // Toggle full screen for element
 // Modified from https://stackoverflow.com/questions/7130397/how-do-i-make-a-div-full-screen
 function toggleFullscreen() {
-// if already full screen; exit
-// else go fullscreen
+    // If already fullscreen, exit fullscreen
     if (
         document.fullscreenElement ||
         document.webkitFullscreenElement ||
@@ -546,7 +545,7 @@ function toggleFullscreen() {
         } else if (document.msExitFullscreen) {
             document.msExitFullscreen();
         }
-    } else {
+    } else { // Not full screen, so go fullscreen
         const element = document.getElementById('render-container');
         if (element.requestFullscreen) {
             element.requestFullscreen();
@@ -558,4 +557,31 @@ function toggleFullscreen() {
             element.msRequestFullscreen();
         }
     }
+}
+
+function handleFullscreen() {
+    if (document.addEventListener)
+    {
+        document.addEventListener('fullscreenchange', handleFullscreenExit, false);
+        document.addEventListener('mozfullscreenchange', handleFullscreenExit, false);
+        document.addEventListener('MSFullscreenChange', handleFullscreenExit, false);
+        document.addEventListener('webkitfullscreenchange', handleFullscreenExit, false);
+    }
+}
+
+function handleFullscreenExit()
+{
+    if (!document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement)
+    {
+        showHide(document.getElementById("fullscreenButton"), true);
+        showHide(document.getElementById("fullscreenClose"), false);
+    } else {
+        showHide(document.getElementById("fullscreenButton"), false);
+        showHide(document.getElementById("fullscreenClose"), true);
+    }
+}
+
+function getComputedProperty(element, property) {
+    const style = getComputedStyle(element);
+    return (parseFloat(style.getPropertyValue(property)));
 }
