727cd42d9c88e950211a1cbcbe7f96de5971522c
Neriderc <89974636+Neriderc@users.noreply.github.com>
2024-01-05 22:50:37 +1300

    Resolve JS issues from previous change
    Javascript errors resolved after hiding shared note option led to scripts finding missing elements.

--- resources/javascript/gvexport.js
+++ resources/javascript/gvexport.js
@@ -210,14 +210,19 @@ function removeSearchOptions() {
     document.getElementById('stop_xref_list').value.split(',').forEach(function (xref) {
         removeSearchOptionFromList(xref, 'stop_pid')
     });
-    // Remove option for shared note if already in list
-    let notes = document.getElementById('sharednote_col_data').value;
-    if (notes !== '') {
-        let json = JSON.parse(notes);
-        json.forEach(item => {
-            removeSearchOptionFromList('@' + item.xref + '@', 'sharednote_col_add');
-        });
-    }
+    isUserLoggedIn().then((loggedIn) => {
+        if (loggedIn) {
+            // Remove option for shared note if already in list
+            let notes = document.getElementById('sharednote_col_data').value;
+            if (notes !== '') {
+                let json = JSON.parse(notes);
+                json.forEach(item => {
+                    removeSearchOptionFromList('@' + item.xref + '@', 'sharednote_col_add');
+                });
+            }
+        }
+    });
+
     // Remove option when searching diagram if indi not in diagram
     let dropdown = document.getElementById('diagram_search_box');
     if (dropdown.tomselect != null) {
@@ -478,7 +483,11 @@ function pageLoaded(Url) {
     document.querySelector(".sidebar_toggle a").addEventListener("click", UI.showSidebar);
     UI.helpPanel.init();
     UI.fixTheme();
-    Form.sharedNotePanel.init();
+    isUserLoggedIn().then((loggedIn) => {
+        if (loggedIn) {
+            Form.sharedNotePanel.init();
+        }
+    });
 
     // Form change events
     const form = document.getElementById('gvexport');
