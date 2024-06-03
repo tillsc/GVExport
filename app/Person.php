645b825b4d12568a09613f1da38bda5463dfbe98
Neriderc <89974636+Neriderc@users.noreply.github.com>
2024-06-03 22:25:00 +1200

    Highlight diagram with new highlight list
    Adds functionality to previous commit that added UI

--- app/Person.php
+++ app/Person.php
@@ -187,7 +187,9 @@ class Person
         }
 
         // Get background colour
-        if ($this->isStartingIndividual($pid) && $this->dot->settings['highlight_start_indis'] == "true" && !$this->isValueInList($this->dot->settings['no_highlight_xref_list'], $pid)) {
+        if ($this->isValueInList($this->dot->settings['highlight_custom'], $pid)) {
+            $indi_bg_colour = $this->dot->settings["highlight_custom_col"];
+        } else if ($this->isStartingIndividual($pid) && $this->dot->settings['highlight_start_indis'] == "true" && !$this->isValueInList($this->dot->settings['no_highlight_xref_list'], $pid)) {
             $indi_bg_colour = $this->dot->settings["highlight_col"];
         } else if ($this->dot->settings["sharednote_col_enable"] && $sharednotes->indiHasSharedNote($pid)) {
             $indi_bg_colour = $sharednotes->getSharedNoteColour($pid);
