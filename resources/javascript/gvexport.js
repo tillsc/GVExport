8c135f5b9da82e826df794c0d474500340142a55
Neriderc <89974636+Neriderc@users.noreply.github.com>
2024-10-29 22:24:10 +1300

    Fix issues
    Fix issues removing indi list items from various lists
    
    Fixed issue with highlight indi list not displaying properly

--- resources/javascript/gvexport.js
+++ resources/javascript/gvexport.js
@@ -327,7 +327,7 @@ function pageLoaded(Url) {
     document.addEventListener("mousedown", function(event) {
 
         // Hide diagram context menu if clicked off a tile
-        if (event.target.closest('.settings_ellipsis_menu_item') == null && event.target.parent.id !== 'menu-info') {
+        if (event.target.closest('.settings_ellipsis_menu_item') == null && event.target.parent && event.target.parent.id !== 'menu-info') {
             UI.contextMenu.clearContextMenu();
         }
     });
